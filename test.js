cbfa95963ff3f926f1c3c8cfeaf3914ac2eba8ca
S.E.Y. Martens <s.martens@ame.nu>
2016-10-05 15:02:39 +0200

    Added some comments

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var Pixelmapper = require('./pixelmapper.js');
 
+// Create a 3x3 zigzag mapping
 var map = new Pixelmapper()
 .zigzag(0, 0, 3, 3, false);
 
+// Convert mapping to compact JSON format that can be used
+// to initialize a the mapping somewhere else
 console.log(new Pixelmapper(map.toJSON())._mapping);
