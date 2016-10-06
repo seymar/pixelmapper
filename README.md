4973ee1d747b09faf3c454035b1d023912675a20
seymar <devcube@me.com>
2016-10-06 21:50:35 +0200

    Better version

---
+++ README.md
@@ -0,0 +1,4 @@
+## Documentation
+
+See [code on github](https://github.com/seymar/pixelmapper/blob/master/pixelmapper.js)
+, every function has good comments.
--- pixelmapper.js
+++ pixelmapper.js
@@ -19,8 +19,8 @@
     this._components = [];
 
     if(typeof components == 'string') {
-      this.fromJSON(components);;
-    } else if(typeof components == 'array') {
+      this.fromJSON(components);
+    } else if(components instanceof Array) {
       this._init(components);
     }
 
@@ -62,19 +62,19 @@
      * Adds a rectangle with zigzag pattern of pixels
      * @param {number} x1 - X start coordinate
      * @param {number} x1 - Y start coordinate
-     * @param {number} dx - Width
-     * @param {number} dy - Height
+     * @param {number} dx - Width, can be negative
+     * @param {number} dy - Height, can be negative
      * @param {boolean} direction - true for horizontal, false for vertical
      */
     Pixelmapper.prototype.zigzag = function(x1, y1, dx, dy, direction) {
       var xl = Math.abs(dx);
       var yl = Math.abs(dy);
 
-      for(var i = 0; i < (direction?dy:dx); i++) {
-        if(direction) {
-          this._line(x1+i, y1+(i%2?dy-1:0), 0, i%2?-dy:dy);
-        } else {
-          this._line(x1+(i%2?dx-1:0), y1+i, i%2?-dx:dx, 0);
+      for(var line = 0; line < (direction?yl:xl); line++) {
+        if(direction) { // Horizontal lines
+          this._line(x1+(line%2?(dx>0?dx-1:dx+1):0), y1+(dy>0?line:-line), line%2?-dx:dx, 0);
+        } else { // Vertical lines
+          this._line(x1+(dx>0?line:-line), y1+(line%2?(dy>0?dy-1:dy+1):0), 0, line%2?-dy:dy);
         }
       }
 
@@ -84,6 +84,40 @@
       });
 
       return this;
+    }
+
+    /**
+     * Adds a number of pixels without an assigned coordinate
+     * Useful for OctoWS12811
+     * @param {number} amount - Amount of pixels
+     */
+    Pixelmapper.prototype.fillEmpty = function(amount) {
+      for(var i = 0; i < amount; i++) this._mapping.push(null);
+    }
+
+    /**
+     * Returns the coordinate assigned to a pixel
+     * @param {number} i - Index of pixel in this._mapping
+     */
+    Pixelmapper.prototype.map = function(i) {
+      if(typeof this._mapping[i] == 'undefined')
+        throw new Error('Pixel with index ' + i + ' does not exist');
+
+      return this._mapping[i];
+    }
+
+    /**
+     * Finds the pixel with given coordinate
+     * @param {number} x
+     * @param {number} y
+     */
+    Pixelmapper.prototype.mapReverse = function(x, y) {
+      for(var i in this._mapping) {
+        if(!this._mapping[i]) continue;
+        if(this._mapping[i][0] == x && this._mapping[i][1] == y)
+          return i;
+      }
+      return false;
     }
 
     /**
@@ -148,6 +182,7 @@
      */
     Pixelmapper.prototype._exists = function(x, y) {
       for(var i in this._mapping) {
+        if(!this._mapping[i]) continue;
         if(this._mapping[i][0] == x && this._mapping[i][1] == y)
           return true;
       }
